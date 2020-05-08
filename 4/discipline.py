10b1d59cccfa9e1e1a430160a4d7a816c171c539
exxxecutor <56535438+exxxecutor@users.noreply.github.com>
2020-05-08 16:01:34 +0300

    Delete discipline.py

--- 4/discipline.py
+++
@@ -1,8 +0,0 @@
-import log
-
-class Discipline:
-    def __init__(self, name, semestr, cafedra):
-        self.name = name
-        self.semestr = semestr
-        self.cafedra = cafedra
-        log.log(log.CRE, f'Создан объект Discipline по адресу {self.__repr__()}')
